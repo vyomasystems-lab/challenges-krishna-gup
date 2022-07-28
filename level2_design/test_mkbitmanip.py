4c006f5deaad3fd1bcc0c9aafa4eb68f7958e978
Krishna <krishna.gupta21100@gmail.com>
2022-07-28 19:17:17 +0000

    Design 2 Checked

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -34,29 +34,40 @@ def run_test(dut):
 
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
-    mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x0
-    mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+
+    # list all possible instr to be checked
+
+    #Failed :.....0x40007033, 
+
+    ins = [0x40007033, 0x40006033, 0x40004033, 0x20001033, 0x20005033, 0x20002033, 0x20004033, 0x20006033, 
+        0x60001033, 0x60005033, 0x48001033, 0x48005033, 0x48006033, 0x48004033, 0x48007033, 0x28001033, 0x28005033,
+        0x68001033, 0x68005033, 0x0A001033, 0x0A003033, 0x0A002033, 0x0A004033, 0x0A005033, 0x0A006033, 
+        0x0A007033, 0x08006033, 0x08004033, 0x08007033, 0x08001033]
+
+    for i in ins:
+        mav_putvalue_src1 = 0x5
+        mav_putvalue_src2 = 0x0
+        mav_putvalue_src3 = 0x0
+        mav_putvalue_instr = i
 
     # expected output from the model
-    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+        expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
 
     # driving the input transaction
-    dut.mav_putvalue_src1.value = mav_putvalue_src1
-    dut.mav_putvalue_src2.value = mav_putvalue_src2
-    dut.mav_putvalue_src3.value = mav_putvalue_src3
-    dut.EN_mav_putvalue.value = 1
-    dut.mav_putvalue_instr.value = mav_putvalue_instr
+        dut.mav_putvalue_src1.value = mav_putvalue_src1
+        dut.mav_putvalue_src2.value = mav_putvalue_src2
+        dut.mav_putvalue_src3.value = mav_putvalue_src3
+        dut.EN_mav_putvalue.value = 1
+        dut.mav_putvalue_instr.value = mav_putvalue_instr
   
-    yield Timer(1) 
+        yield Timer(1) 
 
     # obtaining the output
-    dut_output = dut.mav_putvalue.value
+        dut_output = dut.mav_putvalue.value
 
-    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
-    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+        cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+        cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
     
     # comparison
-    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
-    assert dut_output == expected_mav_putvalue, error_message
+        error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+        assert dut_output == expected_mav_putvalue, error_message
